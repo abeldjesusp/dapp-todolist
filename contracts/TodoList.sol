4b3233a73d59b668673848aba43a87065bcff94a
Abel De Jesus <adejesus@contractor.appsbees.com>
2021-12-16 19:52:57 -0400

    2. List Tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // LIKE AN ASSOCIATE ARRAY
+    // KEY => VALUE
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Learn solidity");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
