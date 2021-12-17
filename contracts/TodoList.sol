367603779e9809d68c767f832399cde2fcb51382
Abel De Jesus <adejesus@contractor.appsbees.com>
2021-12-16 20:21:28 -0400

    3. Creates tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -13,6 +13,12 @@ contract TodoList {
     // KEY => VALUE
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
     constructor() public {
         createTask("Learn solidity");
     }
@@ -20,5 +26,6 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 }
